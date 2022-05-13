0318fbde930048152b106ce8bc0a3697a63519ad
Min Somai <minsomai12345@gmail.com>
2022-05-13 23:21:19 +0545

    Fix: update readme

--- README.md
+++ README.md
@@ -1,45 +1,7 @@
 # vue-flowchart-builder
 
-Attempt to build flowchart builder, based on composite-pattern.
+[Work In Progress] Not Ready For Production use.
 
-Vue 3 in Vite.
+Flowchart builder, based on composite-pattern.
 
 ![Screenshot](screenshot.png)
-
-## Recommended IDE Setup
-
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.vscode-typescript-vue-plugin).
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vitejs.dev/config/).
-
-## Project Setup
-
-```sh
-npm install
-```
-
-### Compile and Hot-Reload for Development
-
-```sh
-npm run dev
-```
-
-### Compile and Minify for Production
-
-```sh
-npm run build
-```
-
-### Run Unit Tests with [Vitest](https://vitest.dev/)
-
-```sh
-npm run test:unit
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-npm run lint
-```
--- src/composables/useFlowchartBuilder.js
+++ src/composables/useFlowchartBuilder.js
@@ -12,14 +12,14 @@ export const useFlowchartBuilder = () => {
         type: "single",
         schema: {
           symbol: "start",
-          id: "5df7e663-09c5-4af5-bdcc-bd0e38548f04",
-        },
+          id: "5df7e663-09c5-4af5-bdcc-bd0e38548f04"
+        }
       },
       {
         type: "single",
         schema: {
           symbol: "process",
-          id: "69753206-dc83-4de0-8ade-76436888fead",
+          id: "67765332-0b02-477e-b378-bb0b36ca6a35",
           children: {
             type: "group",
             schema: {
@@ -30,110 +30,41 @@ export const useFlowchartBuilder = () => {
                   type: "single",
                   schema: {
                     symbol: "process",
-                    id: "67765332-0b02-477e-b378-bb0b36ca6a35",
-                    children: {
-                      type: "group",
-                      schema: {
-                        symbol: "decision",
-                        id: "a4038ea0-62bb-46a5-8396-7911cc6b433d",
-                        children: [
-                          {
-                            type: "single",
-                            schema: {
-                              symbol: "process",
-                              id: "67765332-0b02-477e-b378-bb0b36ca6a35",
-                              isEnd: true,
-                            },
-                          },
-                          {
-                            type: "single",
-                            schema: {
-                              symbol: "process",
-                              id: "fe764f24-6e72-4d25-b547-4a12d2a4accf",
-                              isEnd: true,
-                            },
-                          },
-                        ],
-                      },
-                    },
-                  },
+                    id: "67765332-0b02-477e-b378-bb0b36ca6a39",
+                    isEnd: true
+                  }
                 },
                 {
                   type: "single",
                   schema: {
                     symbol: "process",
                     id: "fe764f24-6e72-4d25-b547-4a12d2a4accf",
-                    children: {
-                      type: "single",
-                      schema: {
-                        symbol: "process",
-                        id: "a4038ea0-62bb-46a5-8396-7911cc6b433d",
-                        children: {
-                          type: "single",
-                          schema: {
-                            symbol: "process",
-                            id: "a4038ea0-62bb-46a5-8396-7911cc6b433d",
-                            children: {
-                              type: "single",
-                              schema: {
-                                symbol: "process",
-                                id: "a4038ea0-62bb-46a5-8396-7911cc6b433d",
-                                children: {
-                                  type: "group",
-                                  schema: {
-                                    symbol: "decision",
-                                    id: "a4038ea0-62bb-46a5-8396-7911cc6b433d",
-                                    children: [
-                                      {
-                                        type: "single",
-                                        schema: {
-                                          symbol: "process",
-                                          id: "67765332-0b02-477e-b378-bb0b36ca6a35",
-                                          isEnd: true,
-                                        },
-                                      },
-                                      {
-                                        type: "single",
-                                        schema: {
-                                          symbol: "process",
-                                          id: "fe764f24-6e72-4d25-b547-4a12d2a4accf",
-                                          isEnd: true,
-                                        },
-                                      },
-                                    ],
-                                  },
-                                },
-                              },
-                            },
-                          },
-                        },
-                      },
-                    },
-                  },
-                },
-              ],
-            },
-          },
-        },
+                    isEnd: true
+                  }
+                }
+              ]
+            }
+          }
+        }
       },
       {
         type: "single",
         schema: {
           symbol: "process",
-          id: "489dab47-d5e9-45f3-8ac3-09334ea14fc6",
-        },
+          id: "489dab47-d5e9-45f3-8ac3-09334ea14fc6"
+        }
       },
       {
         type: "single",
         schema: {
           symbol: "stop",
-          id: "f21bdf67-4f1e-4029-ba39-0b874b66d699",
-        },
-      },
-    ],
+          id: "f21bdf67-4f1e-4029-ba39-0b874b66d699"
+        }
+      }
+    ]
   });
 
   return {
-    schema,
+    schema
   };
 };
